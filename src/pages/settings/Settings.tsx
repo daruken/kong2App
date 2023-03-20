d13e65b5f6552f981367b8e7a60e321d6d5b844a
HongYeol Jeon <dj.hongten@gmail.com>
2023-03-20 22:21:41 +0900

    feat: 하단탭 메뉴 5개 추가 (#1)
    - 메뉴, 검색, 홈, 공지, 설정

---
+++ src/pages/settings/Settings.tsx
@@ -0,0 +1,13 @@
+import { ScrollView, Text } from 'react-native'
+
+const Settings = () => {
+  return (
+    <ScrollView>
+      <Text>
+        Settings !
+      </Text>
+    </ScrollView>
+  );
+}
+
+export default Settings;
