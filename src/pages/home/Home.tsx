fc69407d97378fe207d670264d3a23bcbb126b70
HongYeol Jeon <dj.hongten@gmail.com>
2023-04-01 23:02:44 +0900

    chore: xcode 14.3 버전 업에 따른 설정 변경 (#3)
    - pages 내 파일들은 Webstorm에서 가이드하는 코드로 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
-import { Image, ScrollView, Text, useColorScheme } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {Image, ScrollView, Text, View} from 'react-native';
 
 const Home = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <ScrollView>
-      <Text style={{ fontSize: 35 }}>Scroll me plz</Text>
-      <Image source={{ uri: "https://fastly.picsum.photos/id/391/200/200.jpg?hmac=eWWXf2lLHPoO-zrpo97Q3ViRD7KFeO4CborOD3CC4AU", width: 64, height: 64 }} />
-      <Image source={{ uri: "https://fastly.picsum.photos/id/400/200/300.jpg?hmac=FD74WIE42b0qUFf-QggfWsoHPJqcGgjSatRvUM9dAws", width: 64, height: 64 }} />
-      <Image source={{ uri: "https://picsum.photos/id/237/200/300", width: 64, height: 64 }} />
-      <Image source={{ uri: "https://picsum.photos/200/300?grayscale", width: 64, height: 64 }} />
+      {/* eslint-disable-next-line react-native/no-inline-styles */}
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <Image
+          source={{
+            uri: 'https://fastly.picsum.photos/id/391/200/200.jpg?hmac=eWWXf2lLHPoO-zrpo97Q3ViRD7KFeO4CborOD3CC4AU',
+            width: 64,
+            height: 64,
+          }}
+        />
+        {/* eslint-disable-next-line react-native/no-inline-styles */}
+        <Text style={{fontSize: 24, marginLeft: 20}}>상품명</Text>
+      </View>
+
+      <Image
+        source={{
+          uri: 'https://fastly.picsum.photos/id/400/200/300.jpg?hmac=FD74WIE42b0qUFf-QggfWsoHPJqcGgjSatRvUM9dAws',
+          width: 64,
+          height: 64,
+        }}
+      />
+      <Image
+        source={{
+          uri: 'https://picsum.photos/id/237/200/300',
+          width: 64,
+          height: 64,
+        }}
+      />
+      <Image
+        source={{
+          uri: 'https://picsum.photos/200/300?grayscale',
+          width: 64,
+          height: 64,
+        }}
+      />
+      <Image
+        source={{
+          uri: 'https://picsum.photos/200/300/?blur',
+          width: 64,
+          height: 64,
+        }}
+      />
     </ScrollView>
   );
-}
+};
 
 export default Home;
--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,16 +1,38 @@
-import { SearchBar } from 'react-native-elements';
-import { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import {SearchBar} from 'react-native-elements';
+import {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 
 const Search = (): React.ReactElement => {
   const [search, setSearch] = useState("");
 
-  const updateSearch = (search: string) => {
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  const updateSearch = async (search: string) => {
+    try {
+      const response = await fetch(
+        'http://localhost:8080/api/v1/yogurts/search',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            keyword: search,
+          }),
+        },
+      );
+      const responseJson = await response.json();
+      setSearch(responseJson);
+    } catch (e) {
+      console.log('Failed to fetch...');
+    }
+
     setSearch(search);
   };
 
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <View style={styles.view}>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <SearchBar
         placeholder="검색어를 입력해주세요"
         onChangeText={updateSearch}
@@ -19,7 +41,7 @@ const Search = (): React.ReactElement => {
       />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   view: {
