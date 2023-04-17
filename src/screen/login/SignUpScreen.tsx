e588010d02ff32c1744c33d091251295f2c4458a
HongYeol Jeon <dj.hongten@gmail.com>
2023-04-17 09:45:35 +0900

    deploy 0.0.7 (#7)
    * feat: 회원 가입 추가
    
    * chore: 알 수 없는 파일
    
    * style: 네비게이션 및 버튼 스타일 수정
    
    - 색상 및 문구도 같이 수정

--- src/screen/login/SignUpScreen.tsx
+++ src/screen/login/SignUpScreen.tsx
@@ -1,76 +1,207 @@
-import * as React from 'react';
-import {Button} from 'react-native-elements';
-import {StyleSheet, TextInput, View} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
-import * as common from '../component/Common';
+import * as React from 'react'
+import {createRef, useState} from 'react'
+import {
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import * as common from '../component/Common'
 
-function SignUpScreen() {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+function SignUpScreen({navigation}: any) {
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [errortext, setErrortext] = useState('')
 
-  // eslint-disable-next-line @typescript-eslint/no-shadow
-  const signIn = async (username: string, password: string) => {
+  const passwordInputRef = createRef()
+
+  const signUp = async () => {
+    setErrortext('')
+
+    if (!email) {
+      Alert.alert('E-Mail 주소를 입력하시기 바랍니다.')
+      return
+    }
+
+    if (!username) {
+      Alert.alert('ID를 입력하시기 바랍니다.')
+      return
+    }
+    if (!password) {
+      Alert.alert('비밀번호를 입력하시기 바랍니다.')
+      return
+    }
+    if (!confirmPassword) {
+      Alert.alert('확인 비밀번호를 입력하시기 바랍니다.')
+      return
+    }
+
+    if (password !== confirmPassword) {
+      setErrortext('사용자 ID 또는 비밀번호가 일치하지 않습니다.')
+      return
+    }
+
     try {
+      console.log('username:' + username)
+      console.log('password:' + password)
+      console.log('email:' + email)
+
       const response = await fetch(
-        common.getAPIHost() + '/api/v1/members/sign-in',
+        common.getAPIHost() + '/api/v1/members/sign-up',
         {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            name: username,
+            email: email,
+            username: username,
             password: password,
           }),
         },
-      );
+      )
 
-      const responseJson = await response.json();
-      await AsyncStorage.setItem('jwt', responseJson.token);
+      navigation.replace('SignInScreen')
     } catch (e) {
-      console.log('Failed to sign-in. error: ' + e);
+      console.log('Failed to sign-in. error: ' + e)
     }
-  };
+  }
 
   return (
-    <View>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        style={styles.input}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        style={styles.input}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Sign in" onPress={() => signIn(username, password)} />
+    <View style={styles.mainBody}>
+      <KeyboardAvoidingView enabled>
+        <View style={styles.SectionStyle}>
+          <TextInput
+            style={styles.inputStyle}
+            onChangeText={Email => setEmail(Email)}
+            placeholder="E-Mail address"
+            placeholderTextColor="#d8d8d8"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordInputRef.current}
+            underlineColorAndroid="#f000"
+            blurOnSubmit={false}
+          />
+        </View>
+        <View style={styles.SectionStyle}>
+          <TextInput
+            style={styles.inputStyle}
+            onChangeText={Username => setUsername(Username)}
+            placeholder="Username"
+            placeholderTextColor="#d8d8d8"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordInputRef.current}
+            underlineColorAndroid="#f000"
+            blurOnSubmit={false}
+          />
+        </View>
+        <View style={styles.SectionStyle}>
+          <TextInput
+            style={styles.inputStyle}
+            onChangeText={Password => setPassword(Password)}
+            placeholder="Password"
+            placeholderTextColor="#d8d8d8"
+            keyboardType="default"
+            onSubmitEditing={Keyboard.dismiss}
+            blurOnSubmit={false}
+            secureTextEntry={true}
+            underlineColorAndroid="#f000"
+            returnKeyType="next"
+          />
+        </View>
+        <View style={styles.SectionStyle}>
+          <TextInput
+            style={styles.inputStyle}
+            onChangeText={ConfirmPassword =>
+              setConfirmPassword(ConfirmPassword)
+            }
+            placeholder="ConfirmPassword"
+            placeholderTextColor="#d8d8d8"
+            keyboardType="default"
+            onSubmitEditing={Keyboard.dismiss}
+            blurOnSubmit={false}
+            secureTextEntry={true}
+            underlineColorAndroid="#f000"
+            returnKeyType="next"
+          />
+        </View>
+        {errortext != '' ? (
+          <Text style={styles.errorTextStyle}> {errortext} </Text>
+        ) : null}
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          activeOpacity={0.5}
+          onPress={signUp}>
+          <Text style={styles.buttonTextStyle}>가입</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
-  );
+  )
 }
 
-export default SignUpScreen;
+export default SignUpScreen
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 23,
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    alignContent: 'center',
   },
-  input: {
-    margin: 15,
+  SectionStyle: {
+    flexDirection: 'row',
     height: 40,
-    borderColor: '#7a42f4',
-    borderWidth: 1,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
   },
-  submitButton: {
-    backgroundColor: '#7a42f4',
-    padding: 10,
-    margin: 15,
+  buttonStyle: {
+    backgroundColor: '#04B4AE',
+    borderWidth: 0,
+    borderColor: '#7DE24E',
     height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
   },
-  submitButtonText: {
+  buttonTextStyle: {
+    color: '#ffffff',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
     color: 'white',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: '#dadae8',
   },
-});
+  registerTextStyle: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
+})
